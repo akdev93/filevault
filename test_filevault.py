fab4251368b1546833fcae4cd9311b8b4b508bba
unknown <Abhilash Koneri>
2024-03-31 11:28:23 -0500

    added test for open with invalid paths

--- test_filevault.py
+++ test_filevault.py
@@ -8,3 +8,7 @@ class TestVaultCommandMethods(unittest.TestCase):
         with self.assertRaises(ValueError):
             vc.open([])
 
+    def test_open_InvalidPaths(self):
+        vc = VaultCommands()
+        vc.open(["/non-existing-path", "no-such-key"])
+        self.assertTrue(vc.vault is None)
